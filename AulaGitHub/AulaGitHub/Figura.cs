d3356eb4613b18e39d0f9db4aa2f15233b204d47
carloseduardo9959 <50346234+carloseduardo9959@users.noreply.github.com>
2019-06-04 21:45:04 -0300

    Classe Figura

---
+++ AulaGitHub/AulaGitHub/Figura.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaGitHub
+{
+	class Figura
+	{
+		private string cor;
+		private string nome;
+
+		public string Cor { get => cor; set => cor = value; }
+		public string Nome { get => nome; set => nome = value; }
+
+		virtual public double Area()
+		{
+			return 0;
+		}
+
+		public string GetFigura()
+		{
+			string aux = " ";
+			aux= "Nome: " +Nome +"   Cor: " + Cor;
+			return aux;
+		}
+	}
+}
