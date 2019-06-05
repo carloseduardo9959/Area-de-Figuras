26af3e8229f4973fad5890eea8bf57b8fb37d2bb
Wallace Maia <37050405+wallacemaiag@users.noreply.github.com>
2019-06-04 21:46:45 -0300

    Alteração Circulo

---
+++ AulaGitHub/AulaGitHub/Circulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaGitHub
+{
+    class Circulo : Figura
+    {
+        private double raio;
+
+        public double Raio { set => raio = value; }//OLOKINHO MEU
+
+        public double Area()
+        {
+            double A;
+            A = 3.1416 * (raio * raio);
+            return A;
+        }
+
+    }
+}
