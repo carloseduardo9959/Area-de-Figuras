341f22e1f5702ed84ecda357a1e5b1866b4351a8
carloseduardo9959 <50346234+carloseduardo9959@users.noreply.github.com>
2019-06-04 22:02:32 -0300

    Correções de bugs

--- AulaGitHub/AulaGitHub/Program.cs
+++ AulaGitHub/AulaGitHub/Program.cs
@@ -10,9 +10,16 @@ namespace AulaGitHub
     {
         static void Main(string[] args)
         {
+			List<Figura> LF = new List<Figura>();
+
+			Triangulo T = new Triangulo();
+			Circulo C = new Circulo();
+			Quadrado Q = new Quadrado();
+
 			int Op = 0;
 			do
 			{
+				Console.Clear();
 				Console.WriteLine("===== MENU DE OPÇÕES =====");
 				Console.WriteLine("[1] - Adicionar Quadrado");
 				Console.WriteLine("[2] - Adicionar Triangulo");
@@ -22,6 +29,16 @@ namespace AulaGitHub
 				Console.WriteLine("==========================");
 				Console.Write("Sua opção: ");
 				Op = int.Parse(Console.ReadLine());
+
+				switch (Op)
+				{
+					case 1:
+						Console.Clear();
+						Console.WriteLine("======== QUADRADO =======");
+						Console.WriteLine("Nome: ");
+						
+						break;
+				}
 			} while (Op!=5);
         }
     }
